0f3a8ebc2e43c97376fbce4210596b1bb7d3127c
angiecaterin278 <angiecaterin278@gmail.com>
2026-03-21 17:51:11 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,50 +1,62 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
-from bson import ObjectId
 
 app = FastAPI()
 
+# 🔐 Conexión a Mongo Atlas
+client = MongoClient("mongodb+srv://angie28_db:Angie123@cluster0.i2jqedd.mongodb.net/?retryWrites=true&w=majority")
 
-client = MongoClient("TU_URL_DE_MONGO_ATLAS")
-db = client["curso_python"]
-coleccion = db["usuarios"]
+# 📦 Base de datos y colección
+db = client["Angie"]
+coleccion = db["Usuarios"]
 
 
 @app.get("/")
 def inicio():
-    return {"mensaje": "API funcionando "}
+    return {"mensaje": "API funcionando"}
 
 
 @app.get("/usuarios/{nombre}/{edad}/{correo}")
 def filtrar_usuarios(nombre: str, edad: int, correo: str):
-    filtros = {
-        "nombre": nombre,
-        "edad": edad,
-        "email": correo
-    }
-    usuarios = []
-    for u in coleccion.find(filtros):
-        u["_id"] = str(u["_id"])
-        usuarios.append(u)
-    if usuarios:
-        return usuarios
-    return {"mensaje": "No se encontraron usuarios con esos datos"}
+    try:
+        filtros = {
+            "nombre": nombre,
+            "edad": edad,
+            "email": correo
+        }
+
+        usuarios = []
+        for u in coleccion.find(filtros):
+            u["_id"] = str(u["_id"])
+            usuarios.append(u)
+
+        if usuarios:
+            return usuarios
+
+        return {"mensaje": "No se encontraron usuarios"}
+
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.get("/usuarios/crear/{nombre}/{edad}/{correo}/{intereses}")
 def crear_usuario(nombre: str, edad: int, correo: str, intereses: str):
-    
-    lista_intereses = [i.strip() for i in intereses.split(",")]
+    try:
+        lista_intereses = [i.strip() for i in intereses.split(",")]
 
-    usuario = {
-        "nombre": nombre,
-        "edad": edad,
-        "email": correo,
-        "intereses": lista_intereses
-    }
+        usuario = {
+            "nombre": nombre,
+            "edad": edad,
+            "email": correo,
+            "intereses": lista_intereses
+        }
 
-    resultado = coleccion.insert_one(usuario)
-    return {
-        "mensaje": "Usuario creado",
-        "id": str(resultado.inserted_id)
-    }
+        resultado = coleccion.insert_one(usuario)
+
+        return {
+            "mensaje": "Usuario creado",
+            "id": str(resultado.inserted_id)
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
